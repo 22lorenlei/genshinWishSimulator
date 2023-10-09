b2796fe8bc282952b441b9ed8338ad19adb8ff7f
Loren Lei <22lorenlei@gmail.com>
2023-10-09 18:34:44 -0400

    Fixed text display issue. It was referring to an out of scope variable (beyond the function). I just had to include within the main function for it to be referrable.

--- main.py
+++ main.py
@@ -4,32 +4,35 @@ import pygame
 from wanderlustInvocation import chanceCalculator
 
 backgroundColor = (255, 255, 255)
-screen = pygame.display.set_mode((500, 500))
+screenWidth = 500
+screenHeight = 500
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Wish Simulator')
 screen.fill(backgroundColor)
 pygame.font.init()
 
-def drawFont(message):
+def drawFont(message, centerX, centerY, size, R, G, B):
     text = str(message)
-    font1 = pygame.font.SysFont('Arial', 50)
-    text1 = font1.render(text, True, (0, 0, 0))
+    font1 = pygame.font.SysFont('Times', size)
+    text1 = font1.render(text, True, (R, G, B))
     textRect1 = text1.get_rect()
-    textRect1.center = (250, 250)
+    textRect1.center = (centerX, centerY)
     screen.blit(text1, textRect1)
 
 def main():
     running = True
+    text = "Something"
     while running:
         screen.fill(backgroundColor)
-        message = "something"
-        drawFont(message)
+        message = text
+        drawFont(message, screenWidth / 2, 400, 30, 0, 0, 0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     value = chanceCalculator()
-                    print(value)
+                    text = value
 
         pygame.display.flip()
 
--- wanderlustInvocation.py
+++ wanderlustInvocation.py
@@ -1,10 +1,18 @@
 #for the wanderlustInvocation banner
 import random
+
+fiveStarCharacters = ["Jean", "Qiqi", "Tighnari", "Keqing", "Mona", "Dehya", "Diluc"]
+fiveStarWeapons = ["Amos' Bow", "Skyward Harp", "Lost Prayer to the Sacred Winds", "Skyward Atlas", "Skyward Pride", "Wolf's Gravestone", "Primordial Jade Winged-Spear", "Skyward Spine", "Aquila Favonia", "Skyward Blade"]
+
 def chanceCalculator():
-    chance = random.randint(1, 1000)
-    if chance < 110 and chance > 10:
-        return "4 star"+str(chance)
-    elif chance < 10:
+    chance = random.randint(1, 1001)
+
+    #For five stars (1.6 percent)
+    if 0 < chance < 17:
         return "5 star"+str(chance)
+    #For four stars (13 percent)
+    elif 18 < chance < 149:
+        return "4 star"+str(chance)
+    #Everything else (86.4 percent)
     else:
         return "3 star"+str(chance)
