8deab9aecd1ee661a2bb3b384dfa2c84e8c13b14
Loren Lei <22lorenlei@gmail.com>
2023-10-13 22:03:57 -0400

    Code is a messy. Made the simple animation for a wish falling. Slowed down animation a bit. Still working on the splash when it lands.

--- main.py
+++ main.py
@@ -26,43 +26,61 @@ def drawFont(message, centerX, centerY, size, R, G, B):
 def wishCircle(center, radius, R, G, B):
     pygame.draw.circle(screen, (R, G, B), center, radius)
 
+
 def main():
     running = True
     text = "Press Space to Wish"
     wishCount = 0
     wishRed = 0
-    wishBlue = 30
+    wishBlue = 0
     wishGreen = 0
     wishType = ""
+
+    xPos = 70
+    yPos = 70
+
+    radius = 30
     cooldown = False
+    buffer = 0
     while running:
         screen.fill(backgroundColor)
-        if cooldown == True:
-            if wishType == "5 Star":
-                if wishRed < 255 and wishGreen < 215:
-                    wishRed += 1
-                    wishGreen += 1
+        if buffer % 2 == 0:
+            if cooldown == True:
+                if wishType == "5 Star":
+                    if wishRed < 255 and wishGreen < 255:
+                        wishRed += 1
+                        wishGreen += 1
+                    else:
+                        wishRed = 0
+                        wishBlue = 0
+                        wishGreen = 0
+                        cooldown = False
+                elif wishType == "4 Star":
+                    if wishRed < 255 and wishBlue < 255:
+                        wishRed += 1
+                        wishBlue += 1
+                    else:
+                        wishRed = 0
+                        wishBlue = 0
+                        wishGreen = 0
+                        cooldown = False
                 else:
-                    wishRed = 0
-                    wishBlue = 130
-                    wishGreen = 0
-                    cooldown = False
-            elif wishType == "4 Star":
-                if wishRed < 240 and wishBlue < 255:
-                    wishRed += 1
-                    wishGreen += 1
-                else:
-                    wishRed = 0
-                    wishBlue = 130
-                    wishGreen = 0
-                    cooldown = False
-            else:
-                if wishBlue < 200:
-                    wishBlue += 1
-                else:
-                    wishBlue = 130
-                    cooldown = False
-        wishCircle((screenWidth / 2, screenHeight / 2), 30, wishRed, wishGreen, wishBlue)
+                    if wishBlue < 255:
+                        wishBlue += 1
+                    else:
+                        wishBlue = 0
+                        cooldown = False
+        buffer += 1
+        if xPos < screenWidth and yPos < screenHeight and cooldown == True:
+            wishCircle((xPos, yPos), radius, wishRed, wishGreen, wishBlue)
+            xPos += 0.5
+            yPos += 0.5
+        else:
+            cooldown = False
+            for x in range(0, 1000):
+                if x % 3 == 0:
+                    wishCircle((xPos, yPos), radius, wishRed, wishGreen, 130)
+                    radius += 0.5
         message = text
         drawFont(message, screenWidth / 2, 400, 30, 0, 0, 0)
         drawFont("Wishes: "+str(wishCount), 50, 20, 20, 0, 0, 0)
@@ -71,11 +89,14 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and cooldown == False:
+                    xPos = 70
+                    yPos = 70
                     wish, type = chanceCalculator()
                     text = wish
                     wishType = type
                     wishCount += 1
                     cooldown = True
+                    radius = 30
 
         pygame.display.flip()
 
